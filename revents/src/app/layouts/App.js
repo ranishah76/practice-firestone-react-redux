cd5e656724a65344cbe9c0edbe63edc2e6067b0f
Rani Shah <rani76@gmail.com>
2018-08-01 22:49:05 +0100

    New Files

---
+++ revents/src/app/layouts/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re-vents</h1>
+        <Button icon="smile" content="React Button"/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ revents/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+import App from './app/layouts/App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl)
+}
+if (module.hot) {
+  module.hot.accept('./app/layouts/App', () => {
+    setTimeout(render)
+  })
+}
+
+render ();
+
+ registerServiceWorker();
