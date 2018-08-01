e3c9143f3b4d7976ef2a8ffe0fc0bb93c3a1e263
Rani Shah <rani76@gmail.com>
2018-08-01 23:05:19 +0100

    Event Components

--- revents/src/app/layouts/App.js
+++ revents/src/app/layouts/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/eventDashboard/eventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-vents</h1>
-        <Button icon="smile" content="React Button"/>
+        <EventDashboard/>
       </div>
     );
   }
