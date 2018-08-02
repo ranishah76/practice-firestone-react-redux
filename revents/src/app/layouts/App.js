49b2b098cd6d20dd8b2627cb5bb67d0c7005549b
Rani Shah <rani76@gmail.com>
2018-08-02 13:57:23 +0100

    Added Nav

--- revents/src/app/layouts/App.js
+++ revents/src/app/layouts/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react'
 import EventDashboard from '../../features/event/eventDashboard/eventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
-        <EventDashboard/>
+       <NavBar/>
+        <Container className="main">
+         <EventDashboard/>
+        </Container>
       </div>
     );
   }
