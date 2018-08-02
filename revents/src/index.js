93e5803afee910a46879e8cdd2fade7b68c4847f
Rani Shah <rani76@gmail.com>
2018-08-02 16:10:24 +0100

    Routing

--- revents/src/index.js
+++ revents/src/index.js
@@ -4,12 +4,16 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layouts/App';
 import registerServiceWorker from './registerServiceWorker';
-
+import { BrowserRouter } from 'react-router-dom'
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-  ReactDOM.render(<App />, rootEl)
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+     rootEl)
 }
 if (module.hot) {
   module.hot.accept('./app/layouts/App', () => {
